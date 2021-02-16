953bebaa7e3c27f91fa13204f6fb97b928d085cb
ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>
2021-02-15 21:09:28 -0300

    Add multithread support
    Signed-off-by: ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
-use std::fs;
+use std::{fs, thread};
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -8,8 +9,9 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
-        println!("Connection established!");
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -19,8 +21,14 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep  = b"GET /sleep HTTP/1.1\r\n";
+    let hello = ("HTTP/1.1 200 OK\r\n\r\n", "hello.html");
+
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+        hello
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        hello
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
