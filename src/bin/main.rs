1e563c74699d4ac9a21d3ad7c4b64f2ca30f0278
ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>
2021-02-15 22:00:03 -0300

    Finalize multithread implementation
    Signed-off-by: ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,14 +2,16 @@ use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 use std::{fs, thread};
 use std::time::Duration;
+use ws::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
